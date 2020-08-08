0dcac63844c4b22dd062d875230713a5b0dba68a
Rafaela Tasso <atassrafaela@gmail.com>
2020-08-08 15:43:27 -0300

    Criação de proffys de forma automática

--- src/server.js
+++ src/server.js
@@ -36,15 +36,15 @@ const proffys = [
 ]
 
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    return res.render("study.html", { proffys })
 }
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html")
 }
 
 const express = require('express')
@@ -53,10 +53,11 @@ const nunjucks = require('nunjucks')
 
 nunjucks.configure('src/views', {
     express: server,
-    noCache: true
-}
+    noCache: true,
+})
 
-server.use(express.static("public"))
+server
+.use(express.static("public"))
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
